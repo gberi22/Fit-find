bdcee869ea9805a3eb8e08a1e566cb2ef655defa
berro <gberi22@freeuni.edu.ge>
2026-04-11 21:28:31 +0400

    ""

--- src/main/java/com/fitfind/fitfind/FitFindApplication.java
+++ src/main/java/com/fitfind/fitfind/FitFindApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FitFindApplication {
 
     public static void main(String[] args) {
+        // for pr
         SpringApplication.run(FitFindApplication.class, args);
     }
 
