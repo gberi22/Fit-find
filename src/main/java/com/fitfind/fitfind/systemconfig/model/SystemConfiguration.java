a35de14c4db6a490f588f5781e6ae1f1298a8539
gberi-zb <giorgi.beridze@zerobyte.ge>
2026-05-01 02:41:04 +0400

    "created system configuration which will save all the validity data"

---
+++ src/main/java/com/fitfind/fitfind/systemconfig/model/SystemConfiguration.java
@@ -0,0 +1,30 @@
+package com.fitfind.fitfind.systemconfig.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@SequenceGenerator(name = "system_configuration_seq", sequenceName = "system_configuration_seq", allocationSize = 1)
+public class SystemConfiguration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "system_configuration_seq")
+    private Long id;
+
+    private Long tokenValidityMinutes;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
+
+
--- src/main/resources/db/changelog/changes/init_01.sql
+++ src/main/resources/db/changelog/changes/init_01.sql
@@ -11,3 +11,15 @@ CREATE TABLE IF NOT EXISTS client (
     updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS system_configuration
+(
+    id                            BIGSERIAL PRIMARY KEY,
+    token_validity_minutes        BIGINT,
+    created_at                    TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO system_configuration (id, token_validity_minutes)
+SELECT 1, 60
+WHERE NOT EXISTS (SELECT 1 FROM system_configuration);
+
