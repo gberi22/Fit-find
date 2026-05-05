d58c53bbb04f36cdaad2d4c162029d0933f8bfd6
berro <gberi22@freeuni.edu.ge>
2026-05-06 01:06:24 +0400

    "added rate limit to login and login logic"

--- core/src/main/java/com/fitfind/fitfind/security/auth/service/AuthService.java
+++ core/src/main/java/com/fitfind/fitfind/security/auth/service/AuthService.java
@@ -4,6 +4,8 @@ import com.fitfind.fitfind.client.model.Client;
 import com.fitfind.fitfind.client.model.ClientNotFoundException;
 import com.fitfind.fitfind.client.repository.ClientRepository;
 import com.fitfind.fitfind.security.auth.model.AuthRequest;
+import com.fitfind.fitfind.security.ratelimit.model.RateLimitType;
+import com.fitfind.fitfind.security.ratelimit.service.RateLimitService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,9 +20,12 @@ public class AuthService {
     private final ClientRepository clientRepository;
     private final BCryptPasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final RateLimitService rateLimitService;
 
     public String login(AuthRequest request) {
-        Client client = clientRepository.findClientByEmail(request.email().toLowerCase())
+        String email = request.email().toLowerCase();
+        rateLimitService.enforceRateLimit(email, RateLimitType.CLIENT_LOGIN);
+        Client client = clientRepository.findClientByEmail(email)
                 .orElseThrow(() -> new ClientNotFoundException("No account found for: " + request.email()));
 
         if (!passwordEncoder.matches(request.password(), client.getPassword())) {
