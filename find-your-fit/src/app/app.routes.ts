0317b77afd90f4e138a9d6803dc2bd0080072340
berro <gberi22@freeuni.edu.ge>
2026-05-13 18:06:28 +0400

    "app route config handler"

--- find-your-fit/src/app/app.routes.ts
+++ find-your-fit/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => import('@pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('@pages/login/login.component').then((m) => m.LoginComponent),
+  },
+  { path: '**', redirectTo: '' },
+];
