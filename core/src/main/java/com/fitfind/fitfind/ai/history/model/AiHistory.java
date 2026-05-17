b8ce4bdee8c7f7da12931e5912176193429cedcc
berro <gberi22@freeuni.edu.ge>
2026-05-17 13:06:25 +0400

    "created ai history table with enum class"

---
+++ core/src/main/java/com/fitfind/fitfind/ai/history/model/AiHistory.java
@@ -0,0 +1,56 @@
+package com.fitfind.fitfind.ai.history.model;
+
+import com.fitfind.fitfind.ai.model.reqeust.OutfitSuggestionRequest;
+import com.fitfind.fitfind.ai.model.response.OutfitSuggestionResponse;
+import com.fitfind.fitfind.client.model.Client;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CurrentTimestamp;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.Type;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@DynamicInsert
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(setterPrefix = "with")
+@SequenceGenerator(name = "ai_history_seq", sequenceName = "ai_history_seq", allocationSize = 1)
+public class AiHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ai_history_seq")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private Client client;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "jsonb", nullable = false)
+    private OutfitSuggestionRequest request;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "jsonb", nullable = false)
+    private OutfitSuggestionResponse response;
+
+    @CurrentTimestamp
+    private LocalDateTime createdAt;
+}
