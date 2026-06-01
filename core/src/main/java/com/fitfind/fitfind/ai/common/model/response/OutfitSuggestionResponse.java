b396e9f837382acc1f9e7330dbaa3a200653e392
gberi22 <gberi22@freeuni.edu.ge>
2026-06-02 01:29:03 +0400

    tab fix

--- core/src/main/java/com/fitfind/fitfind/ai/common/model/response/OutfitSuggestionResponse.java
+++ core/src/main/java/com/fitfind/fitfind/ai/common/model/response/OutfitSuggestionResponse.java
@@ -5,5 +5,5 @@ import com.fitfind.fitfind.ai.common.model.CategorySuggestions;
 import java.util.List;
 
 public record OutfitSuggestionResponse(
-        List<CategorySuggestions> categories
+    List<CategorySuggestions> categories
 ) { }
