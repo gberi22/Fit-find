f0c50ed3b85a4e1357fc85238ee9c963f205e922
gberi22 <gberi22@freeuni.edu.ge>
2026-06-02 01:15:22 +0400

    placehoplder profile page, with only functionality to logout from site

---
+++ find-your-fit/src/app/pages/profile/profile.component.ts
@@ -0,0 +1,21 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '@auth/auth.service';
+import { NavbarComponent } from '@shared/ui/navbar/navbar.component';
+
+@Component({
+  selector: 'app-profile',
+  imports: [NavbarComponent],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProfileComponent {
+  private readonly auth = inject(AuthService);
+  private readonly router = inject(Router);
+
+  signOut(): void {
+    this.auth.logout();
+    this.router.navigateByUrl('/login');
+  }
+}
