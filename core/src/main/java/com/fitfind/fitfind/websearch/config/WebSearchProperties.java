126616e676d29d807cbfc1c66bb1f9d128d9e95b
f1re-keeper <elkvc0@gmail.com>
2026-06-03 20:45:23 +0400

    added timeout config for web search
    Signed-off-by: f1re-keeper <elkvc0@gmail.com>

--- core/src/main/java/com/fitfind/fitfind/websearch/config/WebSearchProperties.java
+++ core/src/main/java/com/fitfind/fitfind/websearch/config/WebSearchProperties.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.time.Duration;
+
 @Getter
 @Setter
 @ConfigurationProperties(prefix = "web-search-api")
@@ -11,4 +13,8 @@ public class WebSearchProperties {
 
     private String key;
     private String baseUrl;
+    private Duration connectTimeout = Duration.ofSeconds(5);
+    private Duration readTimeout = Duration.ofSeconds(10);
+    private int maxAttempts = 3;
+    private Duration retryBackoff = Duration.ofMillis(500);
 }
