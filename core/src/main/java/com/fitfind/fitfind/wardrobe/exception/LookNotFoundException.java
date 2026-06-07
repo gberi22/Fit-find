39700ccf6013da13192230701145d8f3c231391e
gberi22 <gberi22@freeuni.edu.ge>
2026-06-07 16:09:18 +0400

    added look not found exception + handler

---
+++ core/src/main/java/com/fitfind/fitfind/wardrobe/exception/LookNotFoundException.java
@@ -0,0 +1,7 @@
+package com.fitfind.fitfind.wardrobe.exception;
+
+public class LookNotFoundException extends RuntimeException {
+    public LookNotFoundException(String message) {
+        super(message);
+    }
+}
