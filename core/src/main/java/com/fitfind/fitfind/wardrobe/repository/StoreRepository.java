540094525bc90c66ef76b499a10894623a1a6218
gberi22 <gberi22@freeuni.edu.ge>
2026-06-08 10:50:40 +0400

    add JPA repositories for Look, Product, and Store

---
+++ core/src/main/java/com/fitfind/fitfind/wardrobe/repository/StoreRepository.java
@@ -0,0 +1,7 @@
+package com.fitfind.fitfind.wardrobe.repository;
+
+import com.fitfind.fitfind.wardrobe.model.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreRepository extends JpaRepository<Store, Long> {
+}
