9cac8d2bd21b54dada9a66eae8a01e7d1ccf620f
gberi22 <gberi22@freeuni.edu.ge>
2026-06-08 10:51:59 +0400

    Add look and product response DTOs for saving final fit

---
+++ core/src/main/java/com/fitfind/fitfind/wardrobe/model/response/ProductResponse.java
@@ -0,0 +1,12 @@
+package com.fitfind.fitfind.wardrobe.model.response;
+
+import com.fitfind.fitfind.ai.common.model.enums.ClothingItem;
+
+public record ProductResponse(
+    Long id,
+    String name,
+    String price,
+    String url,
+    ClothingItem category,
+    String storeName
+) { }
