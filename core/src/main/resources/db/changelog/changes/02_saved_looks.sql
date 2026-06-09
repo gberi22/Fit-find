649d05566c4a476681863430f3ac2cfeb8e7948d
gberi22 <gberi22@freeuni.edu.ge>
2026-06-10 03:32:06 +0400

    extra

--- core/src/main/resources/db/changelog/changes/02_saved_looks.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS saved_looks (
-    client_id BIGINT NOT NULL REFERENCES client(id),
-    look_id   BIGINT NOT NULL REFERENCES looks(id) ON DELETE CASCADE,
-    PRIMARY KEY (client_id, look_id)
-);
